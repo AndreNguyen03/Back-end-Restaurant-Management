b6b28c6f465685a8dfec5048ef502578341710b8
giabao6262 <22520111@gm.uit.edu.vn>
2024-12-12 22:20:01 +0700

    add Table Management feature

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-import express from 'express'
-import cors from 'cors'
-import Database from './dbs/init.mongodb.js';
-import dishRouter from './routes/dishRoute.js';
-import customerAuthRouter from './routes/customerAuthRoute.js';
-import cookieParser from 'cookie-parser';
-import authRouter from './routes/authRoute.js';
+import express from "express";
+import cors from "cors";
+import Database from "./dbs/init.mongodb.js";
+import dishRouter from "./routes/dishRoute.js";
+import customerAuthRouter from "./routes/customerAuthRoute.js";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/authRoute.js";
+import tableRouter from "./routes/tableRoute.js";
 
 // app config
-const app = express()
+const app = express();
 
 // middleware
 app.use(express.json()); //
-app.use(cors(
+app.use(
+  cors(
     {
-        origin: ['http://localhost:5173'],
-        methods: ['POST','GET'],
-        credentials: true
+      origin: ["http://localhost:5173"],
+      methods: ["POST", "GET"],
+      credentials: true,
     }
     // set cors như này thì mới gửi token qua được
-));
+  )
+);
 app.use(cookieParser());
 // db connetion
 Database.getInstance();
 
 // api endpoints
-app.use('/api/dish', dishRouter); 
-app.use('/images',express.static('uploads'));
-app.use('/api/cAuth', customerAuthRouter); // customer auth
-app.use('/api/eAuth', authRouter) // employee auth
-app.get('/', (req,res) => {
-    res.send('API working')
-})
+app.use("/api/dish", dishRouter);
+app.use("/api/table", tableRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/cAuth", customerAuthRouter); // customer auth
+app.use("/api/eAuth", authRouter); // employee auth
+app.get("/", (req, res) => {
+  res.send("API working");
+});
 
 export default app;
-
